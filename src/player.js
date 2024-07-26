5649c5be5940239b9ee44ac4f6c6b3b606c02cb5
Casperavr <casper@hhc.earth>
2024-07-26 11:47:33 +0200

    refactor: move createdomelement logic to method

--- src/player.js
+++ src/player.js
@@ -4,35 +4,40 @@ class Player {
         this.positionX = 20;
         this.positionY = 20;
         this.width = 20;
-        this.height = 20;
+        this.height = 10;
 
+        this.createDomElement();
+    }
+
+
+    createDomElement(){
         // create dom element for player
-        this.playerDiv = document.createElement("div");
+        this.domElement = document.createElement("div");
         const boardElement = document.getElementById("board");
 
 
-        this.playerDiv.id = "player";
-        this.playerDiv.style.left = `${this.positionX}vw`;
-        this.playerDiv.style.bottom = `${this.positionY}vh`;
-        this.playerDiv.style.width = `${this.width}vw`;
-        this.playerDiv.style.height = `${this.height}vh`;
+        this.domElement.id = "player";
+        this.domElement.style.left = `${this.positionX}vw`;
+        this.domElement.style.bottom = `${this.positionY}vh`;
+        this.domElement.style.width = `${this.width}vw`;
+        this.domElement.style.height = `${this.height}vh`;
 
 
-        boardElement.appendChild(this.playerDiv);
+        boardElement.appendChild(this.domElement);
     }
 
     moveLeft(){
         console.log("moving left");
         this.positionX-=2;
         console.log(this.positionX)
-        this.playerDiv.style.left = `${this.positionX}vw`;
+        this.domElement.style.left = `${this.positionX}vw`;
     };
     
     moveRight(){
         console.log("moving right");
         this.positionX+=2;
         console.log(this.positionX)
-        this.playerDiv.style.left = `${this.positionX}vw`;
+        this.domElement.style.left = `${this.positionX}vw`;
 
     }
 }
