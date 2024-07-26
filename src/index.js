b559c83a523ea622e095330130f00e5772c6f38b
Casperavr <casper@hhc.earth>
2024-07-26 13:00:06 +0200

    added obstacle functionality

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 // create player instance of player class
-const myPlayer = new Player()
+const myPlayer = new Player();
+// const obstacle1 = new Obstacle();
+const obstArray = [];   // array of obstacle class instances
 
 
 
 
 
-// functionality:
+// FUNCTIONALITY: -----------------------------------------------
 
+// listening for kepresses
 document.addEventListener("keydown", (event) => {
 
     if (event.code === 'ArrowLeft') {
         myPlayer.moveLeft();
-    } else if (event.code === 'ArrowRight') {
+    } else 
+
+    if (event.code === 'ArrowRight') {
         myPlayer.moveRight();
     }
+
 });
 
+// interval for creating obstacles
+setInterval(() => {
+    const newObstacle = new Obstacle(); 
+    obstArray.push(newObstacle);
+}, 2000);
 
 
+setTimeout(() => {
+    setInterval(() => {obstArray.shift()}, 2000)
+}, 8000)
+
+
+
+// setInterval(() => {
+//     obstArray.forEach((obsIns) => {
+//         obsIns.moveDown();
+//     });
+// }, 100);
 
--- src/obstacle.js
+++ src/obstacle.js
@@ -1,19 +1,19 @@
-class Player {
+class Obstacle {
     constructor(){
         // create basic position and size properties
-        this.positionX = 20;
-        this.positionY = 20;
-        this.width = 20;
+        this.positionX = 40;
+        this.positionY = 90;
+        this.width = 3;
         this.height = 10;
 
         this.createDomElement();
     }
 
     createDomElement(){
-        // create dom element for player
+        // create dom element for obstacle
         this.domElement = document.createElement("div");
         
-        this.domElement.id = "player";
+        this.domElement.className = "obstacle";
         this.domElement.style.left = `${this.positionX}vw`;
         this.domElement.style.bottom = `${this.positionY}vh`;
         this.domElement.style.width = `${this.width}vw`;
@@ -24,20 +24,9 @@ class Player {
         boardElement.appendChild(this.domElement);
     }
 
-    moveLeft(){
-        console.log("moving left");
-        this.positionX-=2;
-        console.log(this.positionX)
-        this.domElement.style.left = `${this.positionX}vw`;
-    };
-    
-    moveRight(){
-        console.log("moving right");
-        this.positionX+=2;
-        console.log(this.positionX)
-        this.domElement.style.left = `${this.positionX}vw`;
-
+    moveDown(){
+        this.positionY--;
+        console.log(this.positionY)
+        this.domElement.style.bottom = `${this.positionY}vh`;
     }
 }
-
-
